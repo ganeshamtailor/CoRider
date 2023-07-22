20426a26f42582c2afaa255f73d532e8488c6b3d
Ganesham <ganeshamtailor@gmail.com>
2023-07-23 00:40:36 +0530

    some refactoring

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,44 +1,42 @@
-import { FC } from 'react'
-import { Users2 , MoreVertical, Phone , MailWarning } from "lucide-react";
+import { FC } from "react";
+import { Users2, MoreVertical, Phone, MailWarning } from "lucide-react";
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuGroup,
-    DropdownMenuItem,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-  } from "@/components/ui/dropdown-menu"
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
-interface MenuProps {
-  
-}
+interface MenuProps {}
 
 const Menu: FC<MenuProps> = ({}) => {
   return (
     <DropdownMenu>
-        <DropdownMenuTrigger>
+      <DropdownMenuTrigger>
         <MoreVertical />
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className='bg-white ' align='end'>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="bg-white " align="end">
         <DropdownMenuGroup>
-        <DropdownMenuItem>
-            <Users2 className="mr-2 h-4 w-4" />
-            <span>Profile</span>
+          <DropdownMenuItem>
+            <Users2 className="mr-2 h-5 w-5" />
+            <span className="font-semibold">Members</span>
           </DropdownMenuItem>
           <DropdownMenuSeparator />
-        <DropdownMenuItem>
-            <Phone className="mr-2 h-4 w-4" />
-            <span>Share</span>
+          <DropdownMenuItem>
+            <Phone className="mr-2 h-5 w-5" />
+            <span className="font-semibold">Share Number</span>
           </DropdownMenuItem>
           <DropdownMenuSeparator />
-        <DropdownMenuItem>
-            <MailWarning className="mr-2 h-4 w-4" />
-            <span>Report</span>
+          <DropdownMenuItem>
+            <MailWarning className="mr-2 h-5 w-5" />
+            <span className="font-semibold">Report</span>
           </DropdownMenuItem>
         </DropdownMenuGroup>
-        </DropdownMenuContent>
+      </DropdownMenuContent>
     </DropdownMenu>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
